dc83f1c3232b53889d69456f623658bd3a872ca2
afairley2 <55838836+afairley2@users.noreply.github.com>
2021-03-21 23:55:40 +0000

    Add files via upload
    This is the companionBot Source code

---
+++ Companion-bot-src/publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild QnABot.sln -p:DeployOnBuild=true -p:PublishProfile=.\companion-bot-Web-Deploy.pubxml -p:Password=Qn8b3B3Rgnb9HHaXSEyGT3PkdDs2GGMMtEurMo3aFuHrB4w28NJW7xfqnZKr
+
